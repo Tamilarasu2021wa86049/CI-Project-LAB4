69521ffee60145b5ed9dd7c7bb8946c7165f75a1
Tamilarasu2021wa86049 <2021wa86049@wilp.bits-pilani.ac.in>
2025-07-02 22:18:53 +0530

    new code commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from CI-Project-LAB4!");
+	System.out.println("this is newly updated code!");
     }
 }
